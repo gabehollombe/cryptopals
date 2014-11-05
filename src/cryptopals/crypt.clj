783f3b33dee7f8ad10d991c634b8e5ff6c908fab
Gabe Hollombe <gabe@avantbard.com>
2014-11-05 01:44:20 -0500

    Finishes 1.5

--- src/cryptopals/crypt.clj
+++ src/cryptopals/crypt.clj
@@ -10,7 +10,15 @@
       byte-array
       Hex/encodeHexString))
 
+(defn encrypt-repeating-xor [text k]
+  (let [repeated-key (repeat (/ (count text) (count k)) k)
+        hexed-text (hex text)
+        hexed-key  (hex repeated-key)]
+    (hex-xor hexed-text hexed-key)))
+
 (defn decrypt [ciphertext k]
+  ;; In: ciphertext as string, and key as string
+  ;; Out: decrypted text as string
   (let [key-length (/ (count ciphertext) 2)
         filled-key (hex (repeat key-length (char k)))]
     (String. (dehex (hex-xor ciphertext filled-key)))))
